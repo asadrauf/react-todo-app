b36388f9cb3e7550abc13f893491206b8fa235a8
ARauf Git <umiasad2017@gmail.com>
2020-05-01 07:16:38 -0400

    added state to manage our todos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react';
 import Todos from './components/Todos';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Todos />
-    </div>
-  );
-}
+
+class App extends Component {
+    state = {
+      todos: [
+        {
+          id: 1,
+          title: 'Do something',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'React is awsome',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'I am learning React',
+          completed: false
+        }
+      ]
+    }
+
+    render(){
+    return (
+      <div className="App">
+        <Todos />
+      </div>
+    );
+  }}
 
 export default App;
