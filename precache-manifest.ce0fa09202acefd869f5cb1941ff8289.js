20a1a590b8646d86eafe49b1701391f337b273f8
ARauf Git <umiasad2017@gmail.com>
2020-05-02 02:54:20 -0400

    Updates

---
+++ precache-manifest.ce0fa09202acefd869f5cb1941ff8289.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e61043b9a3e478954cda9e239b4ca61",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "dec971313f51a075a7f8",
+    "url": "/react-todo-app/static/css/main.0fbefffc.chunk.css"
+  },
+  {
+    "revision": "6e6fb4cafa3044701397",
+    "url": "/react-todo-app/static/js/2.b0dc9a8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.b0dc9a8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dec971313f51a075a7f8",
+    "url": "/react-todo-app/static/js/main.c6a9330d.chunk.js"
+  },
+  {
+    "revision": "90d260951a6062b1ba05",
+    "url": "/react-todo-app/static/js/runtime-main.81e907ce.js"
+  }
+]);
