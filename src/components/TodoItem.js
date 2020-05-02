b44d0498bcd0f57d2b85254c4cacd2629524d83e
ARauf Git <umiasad2017@gmail.com>
2020-05-01 21:33:31 -0400

    added style to the todoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
 class TodoItem extends Component {
+    getStyle = () =>{
+       return{
+           background: '#f4f4f4',
+           borderBottom: '1px #ccc dotted',
+           padding: '10px',
+           textDecoration: this.props.todo.completed ?
+           'line-through' : 'none'
+       } 
+       
+    }
     render() {
         return (
-            <div>
-                <p>{this.props.todo.title}</p>
+            
+            <div style={this.getStyle()}>
+                <p>
+                    <input type = "checkbox" />
+                    {this.props.todo.title}
+                </p>
             </div>
         )
     }
